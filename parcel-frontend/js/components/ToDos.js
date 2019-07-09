27afc3fe75e0e916a48f6711c600c0998c23c2be
AlanKostrick <alan@wecancodeit.org>
2019-07-09 12:11:11 -0500

    delete requests

--- parcel-frontend/js/components/ToDos.js
+++ parcel-frontend/js/components/ToDos.js
@@ -5,6 +5,8 @@ export default function ToDos(toDos) {
         return `
                     <li>
                         <h3>${toDo}</h3>
+                        <input class='delete-toDo__id' type='hidden' value="${toDo}">
+                        <button class='delete-toDoId__submit'>&times</button>
                     </li>
                 `;
     }).join('')}
@@ -13,6 +15,11 @@ export default function ToDos(toDos) {
          <section class='add-toDo'>
             <input class='add-toDo__toDoName' type='text' placeholder='Add a toDo!'>
             <button class='add-toDo__submit'>Submit</button>
+        </section>
+
+        <section class='delete-toDo'>
+            <input class='delete-toDo__toDoName' type='text' placeholder='Delete a toDo!'>
+            <button class='delete-toDo__submit'>Submit</button>
         </section>
     `
 
